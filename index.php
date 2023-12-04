6437f7227bdbc564e6dd9c3f839b4bf91f878e91
Kenobi127 <128316551+Kenobi127@users.noreply.github.com>
2023-12-04 17:12:19 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+	//suppliers_parts leaflet
+	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
+		$uri = 'https://';
+	} else {
+		$uri = 'http://';
+	}
+	$uri .= $_SERVER['HTTP_HOST'];
+	header('Location: '.$uri.'/leaflet/');
+	exit;
+?>
+Something is wrong with the XAMPP installation
